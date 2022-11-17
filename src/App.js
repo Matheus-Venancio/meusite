28cabbb4f773b69b5d116a894486239cff3f664b
Matheus Venâncio Cordeiro <matheusvecordeiro@gmail.com>
2022-11-16 23:59:53 -0300

    config logo

--- src/App.js
+++ src/App.js
@@ -369,22 +369,22 @@ function App() {
           <div class="col-a">
             <header>
 
-              <h2>Fale comigo</h2>
+              <h2>Frases que me definem</h2>
             </header>
 
+            <div className='phrases'>
+              <p className='words'>"A imaginação é mais importante que o conhecimento, porque o conhecimento é limitado, ao passo que a imaginação abrange o mundo inteiro."</p>
+              <p className='people'>Albert Einstein</p>
+            </div>
+
+            <div className='phrases'>
+              <p className='words'>"Se você agir sempre com dignidade, pode não melhorar o mundo, mas uma coisa é certa: haverá na Terra um canalha a menos"</p>
+              <p className='people'>Millôr Fernandes</p>
+            </div>
             <div class="content">
 
               <ul>
-                <li><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M21 10C21 17 12 23 12 23C12 23 3 17 3 10C3 7.61305 3.94821 5.32387 5.63604 3.63604C7.32387 1.94821 9.61305 1 12 1C14.3869 1 16.6761 1.94821 18.364 3.63604C20.0518 5.32387 21 7.61305 21 10Z"
-                    stroke="#00856F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  <path
-                    d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z"
-                    stroke="#00856F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-                  Campinas-SP</li>
+                <li><h2>Outors meios:</h2></li>
                 <li><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                   xmlns="http://www.w3.org/2000/svg">
                   <path
@@ -440,7 +440,7 @@ function App() {
 
           <div class="col-a">
             <a href="#" class="logo_footer" id="#home">
-            <img src={require("./img/logo.png")} />
+              <img src={require("./img/logo.png")} />
             </a>
 
             <p>
