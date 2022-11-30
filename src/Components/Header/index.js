47c23d62c19a3c86bfe5680c131ed2bc92e4d040
Matheus Venâncio Cordeiro <matheusvecordeiro@gmail.com>
2022-11-30 20:38:48 -0300

    icon header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 
 import './styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBrain, faCode, faCoffee, faHouse, faPhone, faUser } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header() {
 
@@ -8,14 +10,13 @@ export default function Header() {
     return (
         <nav id="navigation" class="scroll">
             <ul>
-                <li><a href="#home">Início</a></li>
-                <li><a href="#about">Sobre</a></li>
-                <li><a href="#services">Skills</a></li>
-                <li><a href="#">Projetos</a></li>
-                <li><a href="#">Contato</a></li>
+                <li><a href="#home"><FontAwesomeIcon icon={faHouse} /></a></li>
+                <li><a href="#about"><FontAwesomeIcon icon={faUser} /></a></li>
+                <li><a href="#services"><FontAwesomeIcon icon={faBrain}/></a></li>
+                <li><a href="#"><FontAwesomeIcon icon={faCode}/></a></li>
+                <li><a href="#"><FontAwesomeIcon icon={faPhone}/></a></li>
             </ul>
         </nav>
-
 
     )
 
