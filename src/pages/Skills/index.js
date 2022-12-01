56034004b5bc0e87af9d88b2f36573d3d97a366d
Matheus Venâncio Cordeiro <matheusvecordeiro@gmail.com>
2022-12-01 16:00:25 -0300

    initial skills

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -7,13 +7,11 @@ export default function Skills() {
 
     return (
         <section id="home">
-            <div class="rainbow">
-            <img className="logo_mv" src={require("../../img/skills/html.png")} />
+           <div className="container">
+            <div className="box-grid">
+                
             </div>
-
-            <div class="warning">
-                <p>⚠️ Your browser does not support <a href="https://web.dev/css-individual-transform-properties/">@property</a> so the animation won’t work<br />Please use Chrome.</p>
-            </div>
+           </div>
         </section>
     )
 
