a4c92c83bfc807aea587021236a95d676ee8c8be
Matheus Venâncio Cordeiro <matheusvecordeiro@gmail.com>
2022-12-07 15:42:31 -0300

    initial config skills

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -1,27 +1,29 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCode, faDatabase, faMicrochip, faWifi } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import './styles.css';
 
 export default function Skills() {
 
     return (
         <section id="skills">
-         <div className="container">
+            <div className="container">
                 <div className="box-grid">
                     <h2>Front-end delevoper</h2>
-                    <div className="box-left">
-                        <h3>HTML</h3>
-                        <h3>CSS</h3>
-                        <h3>JAVASCRIPT</h3>
-                        <h3>REACT JS</h3>
-                    </div>
+                    <div className="box">
+                        <div className="content">
+                          <h3>  <FontAwesomeIcon icon={faStar} className='green'/> HTML</h3>
+                            <h3> <FontAwesomeIcon icon={faStar} className='pink'/>CSS</h3>
+                            <h3> <FontAwesomeIcon icon={faStar} className='green'/>JAVASCRIPT</h3>
+                            <h3> <FontAwesomeIcon icon={faStar} className='pink'/>REACT JS</h3>
+                        </div>
 
-                    <div className="box-right">
-                        <h3>BOOTSTRAP</h3>
-                        <h3>SASS</h3>
-                        <h3>TAILWIND</h3>
-                        <h3>TYPESCRIPT</h3>
+                        <div className="content">
+                            <h3> <FontAwesomeIcon icon={faStar} className='green'/>BOOTSTRAP</h3>
+                            <h3> <FontAwesomeIcon icon={faStar} className='pink'/>SASS</h3>
+                            <h3> <FontAwesomeIcon icon={faStar} className='green'/>TAILWIND</h3>
+                            <h3> <FontAwesomeIcon icon={faStar} className='pink'/>TYPESCRIPT</h3>
+                        </div>
                     </div>
                 </div>
 
@@ -30,12 +32,12 @@ export default function Skills() {
                     <div className="box-left">
                         <h3>JAVA</h3>
                         <h3>C#</h3>
-                        
+
                     </div>
 
                     <div className="box-right">
-                    <h3>NODE JS</h3>
-                    <h3>LARAVEL</h3>
+                        <h3>NODE JS</h3>
+                        <h3>LARAVEL</h3>
                     </div>
                 </div>
 
@@ -44,12 +46,12 @@ export default function Skills() {
                     <div className="box-left">
                         <h3>REACT NATIVE</h3>
                         <h3>KOTLIN</h3>
-                        
+
                     </div>
 
                     <div className="box-right">
-                    <h3>JAVA</h3>
-                    <h3>FLUTTER</h3>
+                        <h3>JAVA</h3>
+                        <h3>FLUTTER</h3>
                     </div>
                 </div>
 
@@ -58,12 +60,12 @@ export default function Skills() {
                     <div className="box-left">
                         <h3>MYSQL</h3>
                         <h3>MICROSOFT AZURE</h3>
-                        
+
                     </div>
 
                     <div className="box-right">
-                    <h3>FIREBASE</h3>
-                    <h3>AWS</h3>
+                        <h3>FIREBASE</h3>
+                        <h3>AWS</h3>
                     </div>
                 </div>
             </div>
