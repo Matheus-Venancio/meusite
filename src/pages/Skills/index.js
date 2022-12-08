032d0a25861f229b04c3b5530587c435763410f9
Matheus Venâncio Cordeiro <matheusvecordeiro@gmail.com>
2022-12-08 15:18:42 -0300

    finish config skills

--- src/pages/Skills/index.js
+++ src/pages/Skills/index.js
@@ -29,43 +29,49 @@ export default function Skills() {
 
                 <div className="box-grid">
                     <h2>Back-end delevoper</h2>
-                    <div className="box-left">
-                        <h3>JAVA</h3>
-                        <h3>C#</h3>
+                    <div className="box">
+                        <div className="content">
+                          <h3>  <FontAwesomeIcon icon={faStar} className='green'/> JAVA</h3>
+                            <h3> <FontAwesomeIcon icon={faStar} className='pink'/>C#</h3>
+                        </div>
 
-                    </div>
-
-                    <div className="box-right">
-                        <h3>NODE JS</h3>
-                        <h3>LARAVEL</h3>
+                        <div className="content">
+                            <h3> <FontAwesomeIcon icon={faStar} className='green'/>NODE JS</h3>
+                            <h3> <FontAwesomeIcon icon={faStar} className='pink'/>LARAVEL</h3>
+            
+                        </div>
                     </div>
                 </div>
 
                 <div className="box-grid">
                     <h2>Mobile delevoper</h2>
-                    <div className="box-left">
-                        <h3>REACT NATIVE</h3>
-                        <h3>KOTLIN</h3>
+                    <div className="box">
+                        <div className="content">
+                          <h3>  <FontAwesomeIcon icon={faStar} className='green'/> REACT NATIVE</h3>
+                            <h3> <FontAwesomeIcon icon={faStar} className='pink'/>KOTLIN</h3>
+                        </div>
 
-                    </div>
-
-                    <div className="box-right">
-                        <h3>JAVA</h3>
-                        <h3>FLUTTER</h3>
+                        <div className="content">
+                            <h3> <FontAwesomeIcon icon={faStar} className='green'/>JAVA</h3>
+                            <h3> <FontAwesomeIcon icon={faStar} className='pink'/>FLUTTER</h3>
+            
+                        </div>
                     </div>
                 </div>
 
                 <div className="box-grid">
                     <h2>Database</h2>
-                    <div className="box-left">
-                        <h3>MYSQL</h3>
-                        <h3>MICROSOFT AZURE</h3>
+                    <div className="box">
+                        <div className="content">
+                          <h3>  <FontAwesomeIcon icon={faStar} className='green'/>MYSQL</h3>
+                            <h3> <FontAwesomeIcon icon={faStar} className='pink'/>AZURE</h3>
+                        </div>
 
-                    </div>
-
-                    <div className="box-right">
-                        <h3>FIREBASE</h3>
-                        <h3>AWS</h3>
+                        <div className="content">
+                            <h3> <FontAwesomeIcon icon={faStar} className='green'/>AWS</h3>
+                            <h3> <FontAwesomeIcon icon={faStar} className='pink'/>FIREBASE</h3>
+            
+                        </div>
                     </div>
                 </div>
             </div>
