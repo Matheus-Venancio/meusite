6dc83fa5dc4ab1a7303e5cd3aab3e35b840873e1
Matheus Venâncio Cordeiro <matheusvecordeiro@gmail.com>
2022-12-20 16:50:26 -0300

    send email with emailjs

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,31 +1,49 @@
-import React from "react"
+import React, { useRef } from "react"
+import emailjs from '@emailjs/browser';
+import MaskedInput from "react-text-mask";
 
 import './styles.css';
 
 export default function Form() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_cl2twbj', 'template_pho7vsl', form.current, '-xY4uCricOPRCAlaS')
+      .then((result) => {
+        console.log(result.text);
+        console.log("Message send")
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
 
 
-    return (
-        <div className="form">
-        <div className='form_container'>
-          <div className='inputs'>
-            <input type='text' placeholder="Nome" className="input_form" required maxLength="30" />
-          </div>
-          <div className='inputs'>
-            <input type='email' placeholder="E-mail" className="input_form" required maxLength="30" />
-          </div>
-          <div className='inputs'>
-            <input type='number' placeholder="Telefone" className="input_form" required maxLength="11" />
-          </div>
-          <div className='inputs'>
-            <textarea className='text_area' placeholder='digite algo' rows="4" cols="50" maxLength="50"></textarea>
-          </div>
-          <div className='inputs'>
-            <button className='button_form'>Enviar</button>
-          </div>
+  return (
+
+    <form ref={form} onSubmit={sendEmail}>
+      <div className='form_container'>
+        <div className='inputs'>
+          <input type='text' placeholder="Nome" className="input_form" required maxLength="30" name="user_name" />
+        </div>
+        <div className='inputs'>
+          <input type='email' placeholder="E-mail" className="input_form" required maxLength="30" name="user_email" />
+        </div>
+        <div className='inputs'>
+          <input type='number' placeholder="Telefone" className="input_form" required maxLength="11" name="user_phone" />
+        </div>
+        <div className='inputs'>
+          <textarea className='text_area' placeholder='digite algo' rows="4" cols="50" maxLength="50" name="message"></textarea>
+        </div>
+        <div className='inputs'>
+          <input type="submit" value="Send" className='button_form'></input>
         </div>
       </div>
-    )
+    </form>
+
+
+  )
 
 }
 
