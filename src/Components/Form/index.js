67a7a1327a040b060d455fc548c56c7375e445fe
Matheus Venâncio Cordeiro <matheusvecordeiro@gmail.com>
2022-12-20 17:03:06 -0300

    config form

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,10 +1,13 @@
 import React, { useRef } from "react"
 import emailjs from '@emailjs/browser';
-import MaskedInput from "react-text-mask";
+import { IMaskInput } from "react-imask";
 
 import './styles.css';
 
 export default function Form() {
+  function clickSend(){
+    alert("Enviado com sucesso");
+  }
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -31,13 +34,18 @@ export default function Form() {
           <input type='email' placeholder="E-mail" className="input_form" required maxLength="30" name="user_email" />
         </div>
         <div className='inputs'>
-          <input type='number' placeholder="Telefone" className="input_form" required maxLength="11" name="user_phone" />
+          <IMaskInput
+            mask="(00) 0000-0000"
+            placeholder="Telefonei"
+            className="input_form" required name="user_phone"
+          />
+
         </div>
         <div className='inputs'>
-          <textarea className='text_area' placeholder='digite algo' rows="4" cols="50" maxLength="50" name="message"></textarea>
+          <textarea className='text_area' placeholder='digite algo' rows="4" cols="50" maxLength="50" name="message" required></textarea>
         </div>
         <div className='inputs'>
-          <input type="submit" value="Send" className='button_form'></input>
+          <input type="submit" value="Send" className='button_form' onClick="clickSend()"></input>
         </div>
       </div>
     </form>
